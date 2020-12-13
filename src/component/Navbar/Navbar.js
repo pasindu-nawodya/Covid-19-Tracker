1920aa4d8526c6d2d70971e87dd60640fcea3151
Pasindu Nawodya <53795224+pasindu-nawodya@users.noreply.github.com>
2020-12-13 19:17:48 +0530

    Update Navbar.js

--- src/component/Navbar/Navbar.js
+++ src/component/Navbar/Navbar.js
@@ -4,8 +4,12 @@ import covid from './covid.png';
 export default class NavBar extends Component {
     render() {
 
-        const date = new Date();
-        const today = date.toDateString();
+        var today = new Date();
+        var dd = String(today.getDate()).padStart(2, '0');
+        var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+        var yyyy = today.getFullYear();
+
+        today = mm + '/' + dd + '/' + yyyy;
 
         return (
             <div>
